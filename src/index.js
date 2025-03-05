012ee8cc44f34d12772e744a6ba92210eeaf0715
Anderson <anderson.alanfarias@gmail.com>
2025-03-05 20:39:11 -0300

    add routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import pool from "./config/db.js"
 
+import userRoutes from "./routes/userRoutes.js"
+import errorHandling from "./middlewares/errorHandler.js"
+
 dotenv.config();
 
 const app = express();
@@ -13,8 +16,12 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+app.user("/api", userRoutes);
+
 
 //Error handling middlewares
+app.use(errorHandling)
+
 
 // Testing Postgres connection
 app.get("/", async (req, res) => {
@@ -26,3 +33,6 @@ app.get("/", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http:localhost:${port}`);
 });
+
+
+export default errorHandling;
